812eab5648b934c9f037bce34b8be5a7f1b657ed
Ben Allfree <ben@benallfree.com>
2014-10-11 04:18:38 -0700

    Local resources

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -9,18 +9,18 @@
 
     <title>My App</title>
 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="/lib/jquery-2.1.1.min.js"></script>
     <link rel="stylesheet" href="/css/style.css">
     
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/lib/bootstrap-3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/lib/font-awesome-4.2.0/css/font-awesome.min.css">
     
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="/lib/aFarkas-html5shiv-3.7.2/dist/html5shiv.min.js"></script>
+      <script src="/lib/Respond-1.4.2/dest/respond.min.js"></script>
     <![endif]-->
   </head>
 
@@ -44,7 +44,7 @@
     </div> <!-- /container -->
 
     
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="/lib/bootstrap-3.2.0/js/bootstrap.min.js"></script>
     @yield('footer')
   </body>
 </html>
